6e6450be3e9fdc7eb4dc53d6d9602b3fded4c9d6
joaovictorjesus <70115129+joaovictorjesus@users.noreply.github.com>
2021-08-30 21:19:24 -0400

    ServerSocket

---
+++ Cliente/src/cliente/Cliente.java
@@ -0,0 +1,9 @@
+package cliente;
+
+public class Cliente {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Server/src/server/Server.java
@@ -0,0 +1,26 @@
+package server;
+
+import java.net.ServerSocket;
+
+public class Server {
+    /*1 - Criar o servidor de conexões;
+          2 - Esperar um pedido de conexão;
+            Outro processo
+          2.1 - Criar uma nova conexão;
+          3 - Criar streams de entrada e saida
+          4 - Tratar a conversação entre clientes e servidor (tratar protocolo);
+          4.1 - Fechar socket de comunicação entre servidor/cliente;
+          4.2 - Fechar streams de entrada e saida
+          5 - Voltar para o passo 2, até que finalize o programa;
+          6 - Fechar o ServerSocket
+        */
+        
+    ServerSocket
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
